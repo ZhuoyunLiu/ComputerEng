c1cf8473780c4cd94aa41da9e413b6d58f8d932b
ZhuoyunLiu <1063116772@qq.com>
2014-06-03 00:18:40 +0800

    解决只能读取部分数据的问题
    之前的代码只能读取部分数据，但不管怎么修改，还是只有部分数据读到了，然后我又随便自己写了一个同样多数据的数据集，却可以读取，于是我把eco.txt中没读到的数据重新输入了，然后问题才解决，估计是因为eco.txt的数据我直接从pdf文件里沾过来的，自动转换时有的数据出现问题了

--- C编程实现/APC.c
+++ C编程实现/APC.c
@@ -7,9 +7,9 @@
 typedef struct
 {
 	int idx[R]; //the index of the cluster center for each data point,which shows the economic statistics in 10 years of an economic region of China
-	float dpsim;//the sum of the similarities of the data points to their cluster centers.
-	float expref;// the sum of the preferences of the identified cluster centers.
-	float netsim;//sum of the data point similarities and preferences
+	double dpsim;//the sum of the similarities of the data points to their cluster centers.
+	double expref;// the sum of the preferences of the identified cluster centers.
+	double netsim;//sum of the data point similarities and preferences
 	
 }APinfo;
 
@@ -18,48 +18,46 @@ float data[R][Y];
 int main()
 {
 	
-	FILE *fp,*fpR;
+	FILE *fp;
 	int i,j;
+	
 		for(i=0;i<R;i++)
 		{
 			for(j=0;j<Y;j++)
-			data[i][j]=0.0;
+			   data[i][j]=0.0;
 		}
 	//load the file 
 		
-	fpR=fopen("res.txt","w");
-	fp=fopen("eco.txt","r+");
-	if((fp==NULL)||(fpR==NULL))
+	//fpR=fopen("res.txt","w+");
+	fp=fopen("eco.txt","r");
+	if(fp==NULL)
 	{
 		printf("\n cannot open file\n");
 		return 1;
 	}
-		for(i=0;i<R;i++)
+	for(i=0;i<R;i++)
+	{
+        for(j=0;j<Y;j++)
+                fscanf(fp,"%f",&data[i][j]);
+		//fprintf(fpR,"%f ",data[i][j]);
+		fscanf(fp,"\n");
+		if(feof(fp))
 		{
-			for(j=0;j<Y;j++)
-			{
-		
-			//	if (fread(&data[i][j],sizeof(float),1,fp)!=1)
-			//	{
-			fscanf(fp,"%f",&data[i][j]);
-			fprintf(fpR,"%f ",data[i][j]);
-			printf("%f,",data[i][j]);
-					if(feof(fp))
-					{
-						fclose(fp);
-						return;
-					
-					}
-				//	printf("file read error!\n");
-			//	}
-				
-			}
-				fprintf(fpR,"\n");
-				printf("\n");
+				break;		
 		}
-		fclose(fp);
-		
-	fclose(fpR);
+		//		fprintf(fpR,"\n");
+				
+		}
+		printf("Line10:%f %f %f %f %f %f %f %f %f %f\n",data[10][0],data[10][1],data[10][2],data[10][3],data[10][4],data[10][5],data[10][6],data[10][7],data[10][8],data[10][9]);
+	fclose(fp);
+	printf("%d  %d\n",R,Y);
+	for(i=0;i<R;i++)
+	{
+		for(j=0;j<Y;j++)
+		  printf("%f ",data[i][j]);
+  		  printf("\n");
+	}	
+//	fclose(fpR);
 	getchar();
 
 	return 0;
