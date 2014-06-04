9fb217576375dd591706a50a735811df1f5162ed
ZhuoyunLiu <1063116772@qq.com>
2014-06-04 19:21:24 +0800

    提交今天完成的代码
    基本完成代码，准备开始整理说明文档

---
+++ C编程实现/APC17.c
@@ -0,0 +1,355 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+#include<string.h>
+//Author:LiuzhuoYun, 2014-6-1~2014-6-5 ,implement the algorithm APCLUSTER Affinity Propagation Clustering 
+#define R 31 //31  economic regions
+#define Y 10 //statistic in 10 years
+#define MAXI 200
+#define L 0.1 //λ
+#define Threshold 0.0001
+
+typedef enum {false = 0, true = 1} bool;
+float data[R][Y];
+char province[][]={"北京","天津","河北","山西","内蒙古","辽宁","吉林","黑龙江","上海","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东","广西","海南","重庆","四川","贵州","云南","西藏","陕西","甘肃","青海","宁夏","新疆"}; 
+
+int main()
+{
+    void loadData();
+    void sim(float data[][Y],float s[][R],float p[]);
+	void APcluster(float s[][R],float p[],int idx[]);
+	void outputRes(int in[]);
+	int idx[R]; //the index of the cluster center for each data point,which shows the economic statistics in 10 years of an economic region of China	
+	int i,j;
+	float s[R][R],p[R];
+	
+	//////Initialize///
+	for(i=0;i<R;i++)
+	{
+		for(j=0;j<Y;j++)
+		   data[i][j]=0.0;
+	}
+	for(i=0;i<R;i++)
+	{
+		for(j=0;j<R;j++)
+		   s[i][j]=0.0;
+        p[i]=0.0;
+        idx[i]=0;
+	}
+	/////load data to 2-d array from the txt file
+	loadData();
+	for(i=0;i<R;i++)
+	{
+		for(j=0;j<Y;j++)
+		  printf("%f ",data[i][j]);
+		printf("\n");
+	}
+	sim(data,s,p);
+	APcluster(s,p,idx);
+	
+	//////output the result
+	for(i=0;i<R;i++)
+	{
+		
+		printf("%d:%d\n",i,idx[i]);
+	}
+	outputRes(idx);
+	getchar();
+
+	return 0;
+}
+
+void loadData()
+{
+    FILE *fp;
+    int i,j;
+    //load  data 
+	//fpR=fopen("res.txt","w+");
+	fp=fopen("eco.txt","r");
+	if(fp==NULL)
+	{
+		printf("\n cannot open file\n");
+	}
+	for(i=0;i<R;i++)
+	{
+        for(j=0;j<Y;j++)
+            fscanf(fp,"%f",&data[i][j]);
+		//fprintf(fpR,"%f ",data[i][j]); //test
+		fscanf(fp,"\n");
+		if(feof(fp))
+		{
+				break;		
+		}
+		//fprintf(fpR,"\n");		//test
+		}
+	
+	fclose(fp);
+} 
+
+/////implement APcluser algorithm
+void APcluster(float s[][R],float p[],int idx[])
+{ 
+    float Max(float a[][R],float s[][R],int i);
+    float maxSum(float z,float r[][R],int k);
+    float lam=L;//λ
+    int i,j,k,k1;
+    int times=0,mx=0;
+	float a[R][R],r[R][R],e[R][R];
+	float sum=0.0,tem=0.0,old=0.0,Rdis=0.0,Rdsum=0.0,Rdaver=0.0,Adis=0.0,Adsum=0.0,Adaver=0.0;
+	bool flag=true;
+     //////Initialize///
+	for(i=0;i<R;i++)
+	{
+		for(j=0;j<R;j++)
+		{
+		   a[i][j]=0.0;
+		   r[i][j]=0.0;
+		   e[i][j]=0.0;
+        }	   
+	}
+	/////iterations////
+	while(flag)
+	{
+	    times++;
+	    printf("times:%d\n ",times);
+	    
+	    Rdsum=0.0;
+	    Adsum=0.0;
+	    ///Responsibility
+       for(i=0;i<R;i++) 
+       {
+          k=0;
+          for(k=0;k<R;k++) 
+          {
+              old=r[i][k];
+              r[i][k]=s[i][k]-Max(a,s,i);       //formula1:r(i,k)<-s(i,k)-max{a(i,k')+s(i,k')}
+              r[i][k]=(1-lam)*old+lam*r[i][k];  //formula2:r(new)=λ*r(new)+(1-λ)*r(old)
+              Rdis=r[i][k]-old; //calculate the change 
+             // printf("Rdis(%d,%d):%f ",i,k,Rdis);
+              Rdsum=Rdsum+Rdis; //sum of the changes
+          }             
+       }   
+       Rdaver=Rdsum/R;  //calculate the average of the sum of the changes 
+          printf("\nRdaver %f , ",Rdaver);
+         ///availabilities 
+        for(j=0;j<R;j++) 
+       {
+          k1=0;
+          while(k1<R)
+          {
+              old=a[j][k1];
+              if(j==k1)
+              {
+                  a[j][k1]=maxSum(0.0,r,k1);
+              }    
+              else
+              {
+                  tem=r[k1][k1]+maxSum(0.0,r,k1);  //formula3: r(k,k)+sum of max{0,r(i',k)}
+                  a[j][k1]=(0.0<tem)?0.0:tem;      //formula4:min{0,formula3}
+              }    
+              
+              a[j][k1]=(1-lam)*old+lam*a[j][k1];  //formula:r(new)=λ*r(new)+(1-λ)*r(old)
+              Adis=a[j][k1]-old;  //calculate the change 
+              Adsum=Adsum+Adis;   //sum of the changes
+              k1++;
+          }   //while                   
+       }  //for
+       
+        Adaver=Adsum/R;  //calculate the average of the sum of the changes    
+        printf("\nAdaver %f , ",Adaver);
+        if(((Rdaver*Rdaver)<Threshold) && ((Adaver*Adaver) <Threshold)||(times>MAXI))	
+          flag=false;
+     }
+     
+     for(i=0;i<R;i++)
+     {
+   
+         for(j=0;j<R;j++)
+         {
+             e[i][j]=a[i][j]+r[i][j];
+
+         }  
+         
+     }   
+	  for(i=0;i<R;i++)
+     {
+         mx=0;  
+        
+        for(j=0;j<R;j++)
+         {
+             if(e[i][j]>e[i][mx])
+             {
+               	  mx=j;
+             }  
+			 	idx[i]=mx;  
+		 }
+
+        
+         
+     }   
+}
+///calculate the similarity matrix and diagonal array according to the data
+void sim(float data[][Y],float s[][R],float p[])
+{
+    float median(float t[],int r);
+    int i,k,j;
+    float temp=0.0,min,t[R-1],tm1=0.0,tm2=0.0;
+    for(i=0;i<R-1;i++)
+    {
+        t[i]=0.0;
+    }
+    for(i=0;i<R;i++)   
+    {
+        for(k=0;k<R;k++)
+        {
+            if(i!=k)
+            {
+            	temp=0;
+                for(j=0;j<Y;j++)
+                {
+                	tm1=(float)(data[i][j]-data[k][j]);
+                	tm2=(float)tm1*tm1;
+                    temp=(float)temp+tm2;
+                }    
+                s[i][k]=-temp;
+               // 
+            }  //if  
+        }    //for
+    }    //for
+    printf("s(29,30) %f \n",s[29][30]);
+    for (k=0;k<R;k++)
+    {
+    	j=0;
+        for(i=0;i<R;i++)
+        {
+        	if(i!=k)
+        	{
+        			t[j]=s[i][k];
+        			j++;
+        	}
+            
+        }   
+
+     //   printf("\n");  //test
+        s[k][k]=median(t,R-1);
+        p[k]=s[k][k];
+
+     //   printf("p[%d]:%f ",k,p[k]); //test
+    }
+  	 //	   printf("\n"); //test
+     //     for(i=0;i<R-1;i++)//test
+     //      printf("t%d:%f  ",i,t[i]);//test
+     //    printf("\n");//test
+}
+
+//get the median value of array t
+ float median(float t[],int r)
+ {
+     int i,j,k,mid;
+     float tem,min;
+     bool exchange=false;
+     //Buble Sort
+     for(i=0;i<r;i++)  
+     {
+         exchange=false;
+         for(j=r-1;j>=i;j--)
+         {
+         	if(t[j+1]<t[j])
+         	{
+         	     tem=t[j+1];
+                 t[j+1]=t[j];
+                 t[j]=tem;
+                 exchange=true;
+         	}
+         }
+           	if(!exchange)
+         		break;      
+     }  
+          mid=(int)(r)/2;
+          
+         // for(i=0;i<r;i++)
+         //  printf("%d ",t[i]);
+        //   printf("\n");
+          return t[mid];      
+ }    
+ 
+////calculate the formula: max{a(i,k')+s(i,k')}
+ float Max(float a[][R],float s[][R],int i)
+ {
+     int j=0;
+     float sum=0.0,max=(i>0)?(s[i][0]+a[i][0]):(s[i][1]+a[i][1]);
+     while(j<R)
+     {
+     	 sum=0.0;
+         if(j==i) j++;
+         else
+         {
+             sum=s[i][j]+a[i][j];
+             if(sum>max)
+             {
+                 max=sum;
+             }    
+             j++;
+         }    
+     }  
+     return max;  
+ }    
+ 
+ //calculate the sum of max{0,r(i',k)}
+ float maxSum(float z,float r[][R],int k)
+ {
+     int i=0;
+     float max=z,sum=0.0;
+     while(i<R)
+     {
+     	max=z;
+         if(i==k)i++;
+         else
+         {
+             if(r[i][k]>max)
+                max=r[i][k];
+             sum=sum+max;
+             i++;
+         }    
+     }    
+     return sum;
+ } 
+ 
+ void outputRes(int in[])
+ {
+ 	int i,j,k=1;
+	bool flag=true;
+ 	int cmatrix[R][R];
+ 	for(i=0;i<R;i++)
+ 	{
+ 		for(j=0;j<R;j++)
+ 		{
+ 			if(in[i]==j)
+ 			{
+			 	cmatrix[i][j]=1;
+ 			}
+ 			  
+ 		}
+ 	}
+ 	
+ 	for(i=0;i<R;i++)
+ 	{
+ 		if(flag)
+ 			printf("cluster%d:",k);
+ 		for(j=0;j<R;j++)
+ 		{
+			if(cmatrix[i][j]!=0)
+			{
+				printf("%s ",province[i]);
+				flag=true;
+			}
+			else flag=false;
+ 			  
+ 		}
+ 		if(flag)
+ 			printf("\n");
+ 	}
+}
+ 	
+
+    
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,6 @@
-﻿计算机全英课程任务
-1 收集论文文件夹里存放的是我收集的相关论文
-2 C编程文件夹里存放的是我用C语言实现相似性传播的算法
+﻿APcluster algorithm 
+1.收集论文文件夹里存放的是我收集的相关论文
+Some papers are collected in the Paper folder
+2.C编程文件夹里存放的是我用C语言实现相似性传播的算法代码的部分版本
+The second folder, the C program folder ,in which I put some visions of my code recording the programming progress.
+3. 
